d4ded2516e9f2e4c02a2294f426821a82efa3aff
Tatiana Lapshina <tatianalapshina@protonmail.com>
2021-03-09 20:11:56 -0500

    Added array flattenner

---
+++ arrayflatenner.py
@@ -0,0 +1,25 @@
+'''
+This code below will flatten array of numbers:
+example: (1,18,(19,6,(100,32)))-->[1, 18, 19, 6, 100, 32]
+final array will be a list 
+'''
+from threading import Thread
+
+a = [[1,2,[3,8]],4,12,[72, 94, [11,12,[1,2,[18,[19,[1]]]]]]]
+l = (1,18,(19,6,(100,32)))
+#c = {'car':{'first':'honda', 'second':'toyota'}}
+
+b=[]
+def flattenElement(element):
+    if hasattr(element,'__len__') and not isinstance(element, str):
+        for i in range(len(element)):
+            thread=Thread(target=flattenElement, args=[element[i]])
+            thread.start()
+            thread.join()
+    else:
+        b.append(element)
+        print (b)
+    return b
+
+flattenElement(l)
+#flattenElement(a)
